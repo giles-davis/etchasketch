ca2739393f343c67dd45ff171a16a54bc222fdaf
Mitch Marshall <mtch.marshall@gmail.com>
2024-11-23 20:11:22 +1100

    Created foundation of HTML, CSS, and JS.

---
+++ script.js
@@ -0,0 +1,45 @@
+const container = document.querySelector("#container");
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+function createGrid(dimension) {
+    const totalSquares = dimension * dimension;
+    for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement('div');
+        square.classList.add("square");
+        container.appendChild(square);
+        square.style.opacity = 0;
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = getRandomColor();
+            let currentOpacity = square.style.opacity;
+            if (currentOpacity < 1) {
+                square.style.opacity = Number(currentOpacity) + 0.1
+            }
+        });    
+    }
+}
+
+createGrid(16);
+
+setBtn.addEventListener('click', () => {
+    let result = prompt('How many cells to a side?', '50');
+    if (result === null) {
+      return;
+    } else if (Number(result) > 0 && Number(result) <= 100) {
+      container.replaceChildren();
+      createGrid(Number(result));
+    } else if (Number(result) > 100) {
+      alert("Please input a number between 1 and 100");
+      return;
+    } else if (Number(result) != Number) {
+      alert("Please input a number between 1 and 100");
+      return;
+    }
+  });
